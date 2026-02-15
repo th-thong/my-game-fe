b483cee52652291976dc842048863eb45891e6da
Trần Thanh Thông <thongtran.dev@gmail.com>
2026-02-15 12:07:59 +0700

    add NavBar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,33 @@
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu"
+import { Button } from "@/components/ui/button"
+
+export function NavBar() {
+  return (
+    <nav className="flex h-14 w-full items-center border-b px-6 bg-background shadow-none">
+      
+      <NavigationMenu>
+
+        <NavigationMenuList className="gap-2">
+          
+          <NavigationMenuItem>
+            <NavigationMenuLink href="/" className={navigationMenuTriggerStyle()}>
+              Home
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+
+        </NavigationMenuList>
+      </NavigationMenu>
+
+      <div className="ml-auto">
+        <Button size="sm">Login</Button>
+      </div>
+      
+    </nav>
+  );
+}
