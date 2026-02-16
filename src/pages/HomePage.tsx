ccec764c02fff53133f2c2a39e51db809ea46b70
Trần Thanh Thông <thongtran.dev@gmail.com>
2026-02-16 20:34:51 +0700

    feat: add banner selection feature

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,40 @@
+import { BannerButton } from "@/components/BannerButton";
+import characterBanner from "@/assets/banner/character.webp";
+import weaponBanner from "@/assets/banner/weapon.webp";
+import standardCharacterBanner from "@/assets/banner/standard_character.webp";
+import standardWeaponBanner from "@/assets/banner/standard_weapon.webp";
+import newbieBanner from "@/assets/banner/newbie.webp";
+import choicebBanner from "@/assets/banner/choice.webp";
+import giveBanner from "@/assets/banner/give.webp";
+import { useState } from "react";
+
 export function HomePage() {
+  const [activeBanner, setActiveBanner] = useState<number>(0);
+
+  const banners = [
+    { id: 0, src: characterBanner },
+    { id: 1, src: weaponBanner },
+    { id: 2, src: standardCharacterBanner },
+    { id: 3, src: standardWeaponBanner },
+    { id: 4, src: newbieBanner },
+    { id: 5, src: choicebBanner },
+    { id: 6, src: giveBanner },
+  ];
+
   return (
-    <div>
-        <div className="flex justify-start">
-          Home Page
+    <div className="flex flex-col pl-4 gap-2 pt-2">
+      {banners.map((banner) => (
+        <div key={banner.id} className="w-40 h-auto relative">
+          <div
+            onClick={() => setActiveBanner(banner.id)}
+            className={`w-full h-full transition-transform duration-150 ease-in-out cursor-pointer ${
+              activeBanner === banner.id ? "scale-115" : "scale-100"
+            }`}
+          >
+            <BannerButton imageSrc={banner.src} />
+          </div>
         </div>
+      ))}
     </div>
   );
 }
